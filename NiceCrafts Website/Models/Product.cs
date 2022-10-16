305a99bf8b0915ab91f74912c69c70d957b5cb47
codingxllama <osamaalchalabi2@gmail.com>
2022-10-15 23:28:52 -0400

    Add project files.

---
+++ NiceCrafts Website/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace NiceCrafts_Website.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Maker { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        public string Url { get; set; }
+        public string Description { get; set; }
+
+
+        public int[] Ratings { get; set; }
+
+        // toString method that overrides the standard procedure
+        public override string ToString() =>
+            JsonSerializer.Serialize<Product>(this);
+
+
+
+
+    }
+}
